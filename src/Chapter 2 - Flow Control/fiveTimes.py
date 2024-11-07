4080bdb681d872e066d0c247d96e9f41d449cc8a
andrew <andrew@andrew.com>
2024-11-07 20:35:14 +0000

    completed chapter 2

---
+++ src/Chapter 2 - Flow Control/fiveTimes.py
@@ -0,0 +1,14 @@
+# print('My name is')
+# for i in range(5):
+#     print('Jimmy Five Times (' + str(i+1) + ')')
+
+# print('My name is')
+# i = 0
+# while i < 5:
+#     print('Jimmy Five Times (' + str(i + 1) + ')')
+#     i = i + 1
+
+for i in range(5, -1, -1):
+    print(i)
+
+
