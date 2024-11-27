e9013c4b1de13835f5c6d1e3dbc9f38fd88d30c8
andrew <andrew@andrew.com>
2024-11-27 13:41:37 +0000

    completed chapter 5 in its entirety. Dictionaries are now complete.

--- src/Chapter 5 - Dictionaries and Structuring Data/fantasyGameInventory.py
+++ src/Chapter 5 - Dictionaries and Structuring Data/fantasyGameInventory.py
@@ -1,14 +1,24 @@
-from operator import countOf
+def addToInventory(inventory, addedItems) -> dict:
+    for i in range(len(addedItems) - 1):
+        if inventory.get(addedItems[i]) is None:
+            inventory[addedItems[i]] = 1
+        else:
+            inventory[addedItems[i]] += 1
 
-Inventory = {
-    'rope': 12, 'torch': 6, 'gold coin': 4,
-    'dagger': 3, 'arrow': 1
-}
+    return inventory
 
 def displayInventory(invent):
     print('Inventory: ')
     for item in invent:
         print(invent[item], item)
     print('Total number of items: ' + str(sum(invent.values())))
+
+Inventory = {
+    'gold coin': 42, 'rope': 1
+}
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+Inventory = addToInventory(Inventory, dragonLoot)
 
 displayInventory(Inventory)
